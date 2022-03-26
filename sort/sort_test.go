fad0d6686ce490dfb96ec4e4a7d9e293e9e91d04
Leon Ding <ding@ibyte.me>
2022-03-26 21:41:52 +0800

    add: test uint in sort mod.

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -57,3 +57,111 @@ func TestSequenceBySorting(t *testing.T) {
 		})
 	}
 }
+
+func TestSequenceByQuick(t *testing.T) {
+	tests := []struct {
+		name     string
+		sequence []float64
+	}{
+		{
+			name:     "float64",
+			sequence: []float64{3.3, 2.2, 1.1, 2, 3, 5.665},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Quick(tt.sequence)
+			t.Log(tt.sequence)
+		})
+	}
+}
+
+func TestSequenceByBubble(t *testing.T) {
+	tests := []struct {
+		name     string
+		sequence []float64
+	}{
+		{
+			name:     "float64",
+			sequence: []float64{3.3, 2.2, 1.1, 2, 3, 5.665},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Bubble(tt.sequence)
+			t.Log(tt.sequence)
+		})
+	}
+}
+
+func TestSequenceByMerge(t *testing.T) {
+	tests := []struct {
+		name     string
+		sequence []float64
+	}{
+		{
+			name:     "float64",
+			sequence: []float64{3.3, 2.2, 1.1, 2, 3, 5.665},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Merge(tt.sequence)
+			t.Log(tt.sequence)
+		})
+	}
+}
+
+func TestSequenceByShell(t *testing.T) {
+	tests := []struct {
+		name     string
+		sequence []float64
+	}{
+		{
+			name:     "float64",
+			sequence: []float64{3.3, 2.2, 1.1, 2, 3, 5.665},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Shell(tt.sequence)
+			t.Log(tt.sequence)
+		})
+	}
+}
+
+func TestSequenceBySelection(t *testing.T) {
+	tests := []struct {
+		name     string
+		sequence []float64
+	}{
+		{
+			name:     "float64",
+			sequence: []float64{3.3, 2.2, 1.1, 2, 3, 5.665},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Selection(tt.sequence)
+			t.Log(tt.sequence)
+		})
+	}
+}
+
+func TestSequenceByInsertion(t *testing.T) {
+	tests := []struct {
+		name     string
+		sequence []float64
+	}{
+		{
+			name:     "float64",
+			sequence: []float64{3.3, 2.2, 1.1, 2, 3, 5.665},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Insertion(tt.sequence)
+			t.Log(tt.sequence)
+		})
+	}
+}
