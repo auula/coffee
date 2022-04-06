56def3af9086554ee3e72ef21e0e6152e01d9a4a
auual <ding@ibyte.me>
2022-04-06 17:38:57 +0800

    add: linked list implement.

--- list/linked_list.go
+++ list/linked_list.go
@@ -1,1 +1,71 @@
 package list
+
+type Node[V any] struct {
+	Perv, Next *Node[V]
+	Value      V
+}
+
+type List[V any] struct {
+	Head, Tail *Node[V]
+	Size       uint
+}
+
+func New[V any]() List[V] {
+	return List[V]{
+		Head: nil,
+		Tail: nil,
+		Size: 0,
+	}
+}
+
+func NewNode[V any](v V) *Node[V] {
+	return &Node[V]{Value: v}
+}
+
+func (list *List[V]) RPush(v V) {
+	node := NewNode(v)
+
+	if list.Size == 0 {
+		list.Head = node
+		list.Tail = node
+	} else {
+		node.Perv = list.Tail
+		list.Tail.Next = node
+		list.Tail = node
+	}
+
+	list.Size += 1
+}
+
+func (list *List[V]) LPush(v V) {
+	node := NewNode(v)
+
+	if list.Size == 0 {
+		list.Head = node
+		list.Tail = node
+	} else {
+		node.Next = list.Head
+		list.Head.Perv = node
+		list.Head = node
+	}
+
+	list.Size += 1
+}
+
+func (list *List[V]) Get(index uint) *Node[V] {
+
+	if index > list.Size {
+		return nil
+	}
+
+	if list.Size == 0 {
+		return nil
+	}
+
+	node := list.Head
+	for i := 0; i < int(index); i++ {
+		node = node.Next
+	}
+
+	return node
+}
---
+++ list/linked_list_test.go
@@ -0,0 +1,39 @@
+package list
+
+import "testing"
+
+func TestList(t *testing.T) {
+	cl := New[int]()
+
+	for i := 0; i < 10; i++ {
+		cl.RPush(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		t.Log(cl.Get(uint(i)).Value)
+	}
+
+	cl.LPush(-1)
+
+	t.Log(cl.Head)
+	t.Log(cl.Tail)
+}
+
+func TestListInsert(t *testing.T) {
+	cl := New[int]()
+
+	for i := 0; i < 10; i++ {
+		cl.RPush(i)
+	}
+
+	pervNode := cl.Get(5)
+	newNode := NewNode(-5)
+
+	newNode.Perv = pervNode
+	newNode.Next = pervNode.Next
+	pervNode.Next = newNode
+
+	for i := 0; i < 12; i++ {
+		t.Log(cl.Get(uint(i)))
+	}
+}
