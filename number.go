ebeaf54a2aadbee2f36fa96431d23f2255b50d16
Leon Ding <ding@ibyte.me>
2022-04-29 19:21:35 +0800

    updated: decimal type to float impl.

--- number.go
+++ number.go
@@ -5,12 +5,12 @@ type Integer interface {
 	int8 | uint8 | int16 | uint16 | int32 | uint32 | int64 | uint64 | int
 }
 
-type Decimal interface {
+type Float interface {
 	comparable
 	float32 | float64
 }
 
 type Number interface {
 	comparable
-	Integer | Decimal
+	Integer | Float
 }
