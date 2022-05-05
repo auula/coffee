ebcaf2e52fde606fe247434cc4b61cd9ce9f85b8
Leon Ding <ding@ibyte.me>
2022-05-05 15:14:30 +0800

    fix: binary search middle algorithm.

--- sort/binary_search.go
+++ sort/binary_search.go
@@ -7,7 +7,7 @@ import (
 func Search[T coffee.Number](sequence []T, val T, len int) int {
 	low, high, middle := 0, len-1, 0
 	for low <= high {
-		middle = low + (high-low)>>1
+		middle = low + ((high - low) >> 1)
 		if sequence[middle] == val {
 			return middle
 		}
