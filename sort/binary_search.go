9857c9508c89ba71e5ca834874808e5bc377e5de
Leon Ding <ding@ibyte.me>
2022-05-06 12:18:51 +0800

    fix: binary search target.

--- sort/binary_search.go
+++ sort/binary_search.go
@@ -4,14 +4,14 @@ import (
 	"github.com/auula/coffee"
 )
 
-func Search[T coffee.Number](sequence []T, val T, len int) int {
+func Search[T coffee.Number](sequence []T, target T, len int) int {
 	low, high, middle := 0, len-1, 0
 	for low <= high {
 		middle = low + ((high - low) >> 1)
-		if sequence[middle] == val {
+		if sequence[middle] == target {
 			return middle
 		}
-		if sequence[middle] < val {
+		if sequence[middle] < target {
 			low = middle + 1
 		} else {
 			high = middle - 1
