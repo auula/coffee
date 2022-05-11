b4b707a3bd0882632c9c614ed7ea2ecdcefda9fc
Leon Ding <ding@ibyte.me>
2022-05-11 17:00:48 +0800

    add linked hash map.

--- example/linked_hash_map.go
+++ example/linked_hash_map.go
@@ -8,19 +8,19 @@ import (
 
 func main() {
 
-	var lmap = coffee.NewLinkedHashMap[string, float64](10)
+	var m = coffee.NewLinkedHashMap[string, float64](10)
 
-	lmap.Put("Go", 91.2)
-	lmap.Put("Java", 100)
-	lmap.Put("Rust", 80.1)
+	m.Put("Go", 91.2)
+	m.Put("Java", 100)
+	m.Put("Rust", 80.1)
 
-	lmap.Put("Go", 92.2)
+	m.Put("Go", 92.2)
 
 	// coffee.ForEach(lmap.Iter(), func(i float64) {
 	// 	fmt.Println(i)
 	// })
 
-	lmap.Range(func(node coffee.Node[string, float64]) {
+	m.Range(func(node coffee.Node[string, float64]) {
 		fmt.Printf("\n key:%s \t value:%f \n", node.Key, node.Value)
 	})
 }
