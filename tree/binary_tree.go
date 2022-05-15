80985a24e5569bd1b5c4082a996510315917620e
Leon Ding <ding@ibyte.me>
2022-05-15 20:53:12 +0800

    add: binary tree order.

--- tree/binary_tree.go
+++ tree/binary_tree.go
@@ -1,11 +1,13 @@
 package tree
 
-type Node[T comparable] struct {
+import "github.com/auula/coffee"
+
+type Node[T coffee.Number] struct {
 	Data                  T
 	LeftChild, RightChild *Node[T]
 }
 
-type BinaryTree[T comparable] struct {
+type BinaryTree[T coffee.Number] struct {
 	Root *Node[T]
 	Size int
 }
@@ -34,4 +36,45 @@ func (bt *BinaryTree[T]) Insert(v T) {
 			node = node.LeftChild
 		}
 	}
+}
+
+func (bt *BinaryTree[T]) Search(v T) *Node[T] {
+	node := bt.Root
+	for node != nil {
+		if v < node.Data {
+			node = node.LeftChild
+		} else if v > node.Data {
+			node = node.RightChild
+		} else {
+			return node
+		}
+	}
+	return nil
+}
+
+func Previous[T coffee.Number](node *Node[T], channel chan *Node[T]) {
+	if node == nil {
+		return
+	}
+	channel <- node
+	Previous(node.LeftChild, channel)
+	Previous(node.RightChild, channel)
+}
+
+func Intermediate[T coffee.Number](node *Node[T], channel chan *Node[T]) {
+	if node == nil {
+		return
+	}
+	Intermediate(node.LeftChild, channel)
+	channel <- node
+	Intermediate(node.RightChild, channel)
+}
+
+func Post[T coffee.Number](node *Node[T], channel chan *Node[T]) {
+	if node != nil {
+		return
+	}
+	Post(node.LeftChild, channel)
+	Post(node.RightChild, channel)
+	channel <- node
 }
